f8332803f49f2659fd520198927f4510efc0d5d0
kozyty <kozyty@gmail.com>
2014-10-08 22:30:19 +0900

    bugfix

--- resources/tumblr.audio.js
+++ resources/tumblr.audio.js
@@ -1,6 +1,9 @@
 $(function(){
-  var audio = $(this).find('a.audio-download');
-  if (audio.size() > 0) {
-    $(this).find('.audio-wrap').html('<audio preload="none" controls src="' + audio.attr('href') + '"></audio>');
-  }
+  $('.stat-text').each(function(){
+    var audio = $(this).find('a.audio-download');
+    if (audio.size() > 0) {
+       $(this).find('.audio-wrap').html('<audio preload="none" controls src="' + audio.attr('href') + '"></audio>');
+    }
+  });
 });
+
